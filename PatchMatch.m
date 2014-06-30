c4c59c7230ef3d2fd5787f076336076b9be3236e
Ikuo Degawa <degawa@ikuwow.com>
2014-06-30 23:52:15 +0900

    Implemented input and output
    and add barbara and lena for test images.

--- PatchMatch.m
+++ PatchMatch.m
@@ -1,4 +1,38 @@
 % PatchMatch.m
 
-function PatchMatch()
+% the Matlab code of PatchMatch algorithm
+
+% PatchMatch returns approximate nearest neighbor field (NNF).
+
+% targetImg: An image (usually masked by NaN. NaN is lost domain)
+% sourceImg: An image from which patches are extracted, same size as targetImg.
+% psz: patch size (psz x psz). Default is 9. 
+
+% NNF: contains indices of sourceImg for each indices of targetImg
+function NNF = PatchMatch(targetImg, sourceImg, psz)
+
+% set psz to default
+if (nargin<3) psz = 9; end
+
+% grayscale images only (TODO: extend to color images)
+if ndims(targetImg) > 2 targetImg = rgb2gray(targetImg); end
+if ndims(sourceImg) > 2 sourceImg = rgb2gray(sourceImg); end
+
+% targetImg = double(targetImg);
+% sourceImg = double(sourceImg);
+
+%%%%%%%%%%%%%%%%%%%%
+%--  Initialize  --%
+%%%%%%%%%%%%%%%%%%%%
+
+NNF = cat(3,randi([1,size(sourceImg,1)],size(targetImg,1)),randi([1,size(sourceImg,2)],size(targetImg,2)));
+
+%%%%%%%%%%%%%%%%%%%%%
+%--  Propagation  --%
+%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%
+%--  RandomSearch  --%
+%%%%%%%%%%%%%%%%%%%%%%
+
 end
--- testPatchMatch.m
+++ testPatchMatch.m
@@ -1,3 +1,15 @@
 % testPatchMatch.m
+% test run of PatchMatch.m
 
+clear all;
+close all;
+
+inImg = imread('barbara.bmp');
+srcImg = imread('lena.bmp');
+% mask = ones(size(inImg,1),size(inImg,2));
+% mask(100:120,200:220) = NaN; 
+
+psz = 9;
+
+NNF = PatchMatch(inImg, srcImg, psz);
 
